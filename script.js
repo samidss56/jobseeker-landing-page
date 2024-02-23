2cb63f4c61e62c3039d7b9510dd7801a01026d29
samidss56 <adimas.id@gmail.com>
2024-02-23 14:37:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showToggler() {
+  const toggler = document.querySelector(".navbar-toggle-menu");
+  toggler.style.display = "flex";
+}
+
+function closeToggler() {
+  const toggler = document.querySelector(".navbar-toggle-menu");
+  toggler.style.display = "none";
+}
